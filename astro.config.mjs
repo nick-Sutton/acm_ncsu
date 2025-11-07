2605b8ab80463e1530c22fe0520e6eee4e4f0664
Nicholas Sutton <nicksutton46@gmail.com>
2025-11-07 11:49:55 -0500

    updated base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nick-Sutton.github.io/acm_ncsu',
+  site: 'https://nick-Sutton.github.io/',
   base: '/acm_ncsu',
   integrations: [mdx(), sitemap(), icon()],
 });
